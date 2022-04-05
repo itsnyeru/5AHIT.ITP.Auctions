a92db62985033b5d644b27530c60f99560cf06fe
Zelinsky Nico <zelinskynico@gmail.com>
2022-04-05 15:18:40 +0200

    auctionpage

--- Domain/Repository/AuctionRepository.cs
+++ Domain/Repository/AuctionRepository.cs
@@ -8,6 +8,9 @@ namespace Domain.Repository;
 public interface IAuctionRepository : IRepositoryAsync<Auction> {
     Task<List<Auction>> All(Auction? lastElement = null);
     Task<List<Auction>> Filter(string title, List<Categorie> categories, Auction? lastElement = null);
+    Task<List<Categorie>> GetCategories();
+    Task<List<Auction>> GetSellerProducts(User user);
+    Task<List<Auction>> GetSimiliarProducts(Auction auction);
 }
 
 public class AuctionRepository : ARepositoryAsync<Auction>, IAuctionRepository {
@@ -26,10 +29,21 @@ public class AuctionRepository : ARepositoryAsync<Auction>, IAuctionRepository {
             .Include(o => o.Buyer)
             .Where(o => o.Buyer == null)
             .Where(o => string.IsNullOrWhiteSpace(title) ? true : o.Title.ToLower().Contains(title.ToLower()))
-            .Where(o => !o.Categories.Any() ? true : CombineCategories(o.Categories.Select(o => o.Categorie)) == CombineCategories(categories))
-            .SkipWhile(o => lastElement != null && o.Id != lastElement.Id)
+            .Where(o => categories == null || !categories.Any() || !o.Categories.Any() ? true : CombineCategories(o.Categories.Select(o => o.Categorie)) == CombineCategories(categories))
+            .Where(o => o.Id > (lastElement == null ? 0 : lastElement.Id))
             .Take(20)
             .ToListAsync();
     }
+    public async Task<List<Categorie>> GetCategories() => await _context.Set<Categorie>().ToListAsync();
     private string CombineCategories(IEnumerable<Categorie> categories) => categories.Select(c => c.Label).Aggregate((a, b) => a + ";" + b);
+
+    public async Task<List<Auction>> GetSellerProducts(User user) => await _set.Include(o => o.Seller).Where(o => o.Seller.Id == user.Id).ToListAsync();
+    public async Task<List<Auction>> GetSimiliarProducts(Auction auction) => await _set
+            .Include(o => o.Categories).ThenInclude(o => o.Categorie)
+            .Include(o => o.Buyer)
+            .Where(o => o.Buyer == null)
+            .Where(o => auction.Categories.Select(o => o.Categorie).ToList().Any() || !o.Categories.Any()? true : CombineCategories(o.Categories.Select(o => o.Categorie)) == CombineCategories(auction.Categories.Select(o => o.Categorie).ToList()))
+            .Where(o => o.Id != auction.Id)
+            .Take(20)
+            .ToListAsync();
 }
--- SignalR/AuctionHub.cs
+++ SignalR/AuctionHub.cs
@@ -1,10 +1,11 @@
 ﻿
 using Microsoft.AspNetCore.SignalR;
+using Model.Entity;
 
 namespace SignalR;
 
 public class AuctionHub : Hub {
-    public async Task SendBet(int auctionId, string name, decimal price) {
-        await Clients.All.SendAsync($"ReceiveBet-{auctionId}", auctionId, name, price);
+    public async Task SendBet(BiddingAuctionBid bid) {
+        await Clients.All.SendAsync($"ReceiveBet-{bid.Auction.Id}", bid);
     }
 }
